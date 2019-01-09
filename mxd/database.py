c0c78d9398b56d62e7f1a729bc84e564b54d993a
ahn <math.ansang@gmail.com>
2019-01-09 18:11:06 +0900

    start

--- mxd/database.py
+++ mxd/database.py
@@ -1,5 +1,50 @@
 import sqlite3
-import sqlalchemy
+import win32com.client
+import winpaths
+import json
+import os
+
+conn = None
+applicationName = 'test'
+settings = None
 
 
-engine = sqlalchemy.create_engine('sqlite:///:memory:', echo=True)
+def initialize(appname):
+    settings = json.load(os.path.join(winpaths.get_appdata(), 'Montrix', 'MxExcelAddIn'))
+
+    db_filie = os.path.join(settings['repository'], 'BlobCache',appname,'userblobs.db')
+    conn = sqlite3.connect(db_filie)
+
+
+def load_workspace_meta_json(ws_name):
+    #res  = b'{"name": "John Smith", "hometown": {"name": "New York", "id": 123}}'
+
+    sql = "SELECT NAME, DATA FROM TABLE1 WHERE KEY=:key"
+    param = { 'key':  ws_name }
+    cursor = conn.cursor()
+    cursor.execute(sql, param)
+    name, ws_meta  = cursor.fetchone()
+
+    return ws_meta.encode("utf-8")
+
+
+def load_variable_meta_json(ws_name, var_name):
+    sql = "SELECT NAME, DATA FROM TABLE1 WHERE KEY=:key"
+    param = { 'key':  ws_name }
+    cursor = conn.cursor()
+    cursor.execute(sql, param)
+    name, var_meta  = cursor.fetchone()
+
+    return var_meta.encode("utf-8")
+
+# http://www.numericalexpert.com/blog/sqlite_blob_time/
+def load_variable_value(ws_name, var_name):
+    sql = "SELECT NAME, DATA FROM TABLE1 WHERE KEY=:key"
+    param = { 'key':  ws_name + "#" + var_name }
+    cursor = conn.cursor()
+    cursor.execute(sql, param)
+    name, data  = cursor.fetchone()
+
+    return data.encode("utf-8")
+
+
--- mxd/variables.py
+++ mxd/variables.py
@@ -1,16 +1,41 @@
+import numpy as np
+import json
+import database as db
+from io import StringIO
+from collections import namedtuple
 
+
+def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values())
+def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
+
+
+class Workspace:
+    def __init__(self, ws_name):
+        self.meta_ = json2obj(db.load_workspace_meta_json(ws_name))
+
+    def name(self):
+        return self.meta_.name
 
 
 class Variable:
-    def __init__(self):
-        pass
+    def __init__(self, ws_name, var_name):
+        self.meta_ = json2obj(db.load_variable_meta_json(ws_name, var_name))
 
-    def var_type(self):
-        pass
+    def type(self):
+        return self.meta_.type
 
-    # lazy load
+    # lazy load : double or string or npz binary
+    # data = np.genfromtxt(s, dtype=[('myint','i8'),('myfloat','f8'),
+    # ... ('mystring','S5')], delimiter=",")
     def value(self):
+        res = db.load_variable_value(self.meta_.name, self.meta_.name)
 
+        if self.meta_.var_type == 'default':
+            return np.genfromtxt(StringIO(res),delimiter=",")
+        elif self.meta_.var_type == 'double':
+            return np.genfromtxt(StringIO(res),delimiter=",")
+        else:
+            return 'load fail'
 
 
 class DefaultVariable(Variable):
